b0dc25b8cc7ca8565ad3fd73c3b5db1325efce60
Andrea Puebla <107224227+andreagpuebla@users.noreply.github.com>
2024-05-15 21:44:10 -0300

    Add files via upload

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,17 @@
+//Ejercicio 1 
+
+var nombre= "Andrea"
+var apellido= "Puebla"
+
+console.log (
+   nombre + " " + apellido
+)
+
+//Ejercicio 2 
+var number1 = 1
+var number2 =2
+var suma= number1+ number2
+console.log (suma)
+
+var number4= suma + 10
+console.log (number4)
